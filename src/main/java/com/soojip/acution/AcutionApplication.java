0244fa558f7de13dd844bbebc011e4ba4f22dce7
한치현 <min74650@gmail.com>
2021-01-24 02:39:50 +0900

    init commit

---
+++ src/main/java/com/soojip/acution/AcutionApplication.java
@@ -0,0 +1,13 @@
+package com.soojip.acution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcutionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/soojip/acution/AcutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soojip.acution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcutionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
